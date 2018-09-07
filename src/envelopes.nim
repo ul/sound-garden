2196525e491f625113a00d8cc4b7fb99c2c52a17
Ruslan Prokopchuk <fer.obbee@gmail.com>
2018-09-07 17:10:40 +1000

    fix impulse envelope

--- src/envelopes.nim
+++ src/envelopes.nim
@@ -7,7 +7,7 @@ import std
 
 # NOTE apex is relative to start and is in seconds
 proc impulse*(trigger, apex: Signal): Signal =
-  let startSample = trigger.sampleAndHold(sampleNumber)
+  let startSample = (trigger > 0.0).sampleAndHold(sampleNumber)
   let h = (sampleNumber - startSample) / (apex * sampleRate)
   result = h * maths.exp(1.0 - h)
   result.label = "impulse(" && trigger.label && ", " && apex.label && ")"
